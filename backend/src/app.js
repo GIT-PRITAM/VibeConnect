df10b60715da75b5308cd4a392c89ba3e21395b4
Pritam Sarkar <pritamsarkar9415@gmail.com>
2025-12-19 22:11:38 +0530

    environment variable fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { createServer } from "node:http";
+import dotenv from "dotenv";
 import { Server } from "socket.io";
 import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
@@ -10,20 +11,24 @@ import userRoutes from "./routes/users.routers.js";
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGO_URI;
 
 app.set("port", (process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
+
+
 app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
-    const connectionDb = await mongoose.connect("mongodb+srv://ps0783123:2LEwlTxgchDRycwx@cluster0.6qfseks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    const connectionDb = await mongoose.connect(MONGODB_URI);
 
-    console.log(`Mongo Host: ${connectionDb.connection.host}`);
     server.listen(app.get("port"), () => {
-        console.log("Listening to port 8000");
+        console.log(`Server is running on port ${app.get("port")}`);
     })
 }
 
